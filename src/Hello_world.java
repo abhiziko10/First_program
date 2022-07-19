53201f8f79a5a6ac282d2114d3da49000410c141
Abhishek Banerjee <abhishek.banerjee@coditas.com>
2022-07-20 00:10:59 +0530

    this is my intellij demo

--- src/Hello_world.java
+++ src/Hello_world.java
@@ -1,6 +1,7 @@
 public class Hello_world {
-    public static void main(String args[]){
+    public static void main(String [] args){
         System.out.println("Hello World");
         System.out.println("thid");
+        System.out.println("Coditas is Love");
     }
 }
